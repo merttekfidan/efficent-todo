d4dde755223f52cdaeacf03d3075c3f4156e4960
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-06-10 22:41:43 +0200

    Set the database

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
 const app = require("./app");
 
-const server = app.listen(3000, () => {
-  console.log(`App running on port 3000...`);
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+console.log(DB);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successful!");
+  })
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
