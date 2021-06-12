c64d931797f019c75f5f29965be085544119dc18
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-06-12 23:27:34 +0200

    Implement  create and get methods

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -18,21 +18,33 @@ exports.test = async (req, res) => {
   }
 };
 
-exports.subTest = async (req, res) => {
+exports.createTodo = async (req, res) => {
   try {
-    const todo = await Todo.create({
-      title: "Test2",
-      subTodo: "60c12295d2a87bdd7add7c96",
-    });
-    res.status(200).json({
-      status: "success",
-      data: todo,
-    });
+    if (req) {
+      var todo = await Todo.create(req.body);
+      if (req.body.parentTodo) {
+        //Doesnt EXECUTE, work on findbyidandupdate. Yenisini eklemiyor eskisini güncelliyor.
+        var todo = await Todo.findByIdAndUpdate(
+          req.body.parentTodo,
+          {
+            subTodo: todo._id,
+          },
+          { new: true },
+          (err, doc) => {}
+        );
+      }
+
+      res.status(200).json({
+        status: "success",
+        data: { todo: todo },
+      });
+    }
   } catch (err) {
     console.log(err);
   }
 };
-exports.findTest = async (req, res) => {
+
+exports.getTodos = async (req, res) => {
   try {
     const todo = await Todo.find().populate();
     res.status(200).json({
--- routes/todoRouter.js
+++ routes/todoRouter.js
@@ -3,8 +3,11 @@ const todoController = require("./../controllers/todoController");
 const router = express.Router();
 
 //router.use("/", todoController.sayHello);
-router.use("/test", todoController.test);
-router.use("/subtest", todoController.subTest);
-router.use("/findtest", todoController.findTest);
+// router.use("/test", todoController.test);
+// router.use("/subtest", todoController.subTest);
+// router.use("/findtest", todoController.findTest);
+
+router.route("/todos").get(todoController.getTodos);
+router.route("/createTodo").post(todoController.createTodo);
 
 module.exports = router;
