e90cead39d75cdd9c3b7e6a78b54176741377ee7
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-06-13 05:12:32 +0200

    Refactoring

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,57 +1,49 @@
 const Todo = require("./../models/todoModel");
-exports.sayHello = (req, res) => {
+const catchAsync = require("./../utils/catchAsync");
+
+exports.getTodos = catchAsync(async (req, res, next) => {
+  const todo = await Todo.find();
   res.status(200).json({
     status: "success",
-    data: "Hello",
+    data: todo,
   });
-};
-// todoid:60c122b804d985df59ccaae9
-exports.test = async (req, res) => {
-  try {
-    const todo = await Todo.create({ title: "Test1" });
-    res.status(200).json({
-      status: "success",
-      data: todo,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+});
 
-exports.createTodo = async (req, res) => {
-  try {
-    if (req) {
-      var todo = await Todo.create(req.body);
-      if (req.body.parentTodo) {
-        //Doesnt EXECUTE, work on findbyidandupdate. Yenisini eklemiyor eskisini güncelliyor.
-        var todo = await Todo.findByIdAndUpdate(
-          req.body.parentTodo,
-          {
-            subTodo: todo._id,
-          },
-          { new: true },
-          (err, doc) => {}
-        );
-      }
-
-      res.status(200).json({
-        status: "success",
-        data: { todo: todo },
-      });
+exports.createTodo = catchAsync(async (req, res, next) => {
+  if (req) {
+    // Adds a todo
+    var todo = await Todo.create(req.body);
+    if (req.body.parentTodoId) {
+      // Finds parent todo and adds a sub todo
+      var todo = await Todo.findByIdAndUpdate(
+        req.body.parentTodoId,
+        {
+          $push: { subTodo: todo._id },
+        },
+        { new: true }
+      );
     }
-  } catch (err) {
-    console.log(err);
-  }
-};
 
-exports.getTodos = async (req, res) => {
-  try {
-    const todo = await Todo.find().populate();
+    res.status(201).json({
+      status: "success",
+      data: { todo },
+    });
+  }
+});
+
+exports.flipStatus = catchAsync(async (req, res, next) => {
+  if (req) {
+    const todo = await Todo.findByIdAndUpdate(
+      req.params.id,
+      {},
+      function (err, val) {
+        val.completed = !val.completed;
+        val.save();
+      }
+    );
     res.status(200).json({
       status: "success",
-      data: todo,
+      data: { todo },
     });
-  } catch (err) {
-    console.log(err);
   }
-};
+});
