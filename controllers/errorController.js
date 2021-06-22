7d319186612005b3f58efc30fd4bb6e1f18f2905
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-06-22 21:53:30 +0200

    Exclude production error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -51,8 +51,8 @@ const sendErrorProd = (err, res) => {
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-
-  if (process.env.NODE_ENV === "development") {
+  sendErrorDev(err, res);
+  /*if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
     //Interestingly, you have to define ''name'' property seperately. Otherwise, object destructering will not catch err.name
@@ -65,5 +65,5 @@ module.exports = (err, req, res, next) => {
     if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
 
     sendErrorProd(error, res);
-  }
+  }*/
 };
