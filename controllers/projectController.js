22a450e599c9f41f8789154eb9c8cb5714533cf2
Merdjan Tekfidan <merttekfidan@gmail.com>
2021-06-23 00:59:10 +0200

    Handle errors

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,85 +1,93 @@
 const Project = require("./../models/projectModel");
 const Todo = require("./../models/todoModel");
 const catchAsync = require("./../utils/catchAsync");
-
-const status = async (statusCode, data, res) => {
-  if (statusCode === 404) {
-    res.status(statusCode).json({
-      status: "fail",
-    });
-  } else {
-    res.status(statusCode).json({
-      status: "success",
-      data,
-    });
-  }
-};
+const AppError = require("./../utils/appError");
 
 exports.createProject = catchAsync(async (req, res, next) => {
   const project = await Project.create(req.body);
-  if (project) {
-    status(200, project, res);
-  } else {
-    status(500, null, res);
-  }
+  res.status(201).json({
+    status: "success",
+    data: {
+      data: project,
+    },
+  });
 });
+
 exports.getAllProjects = catchAsync(async (req, res, next) => {
   const projects = await Project.find();
-  if (projects) {
-    status(200, projects, res);
-  } else {
-    status(500, null, res);
-  }
+
+  res.status(200).json({
+    status: "success",
+    results: projects.length,
+    data: { data: projects },
+  });
 });
+
 exports.getAProject = catchAsync(async (req, res, next) => {
   const project = await Project.findOne({ _id: req.params.id });
-  if (project) {
-    status(200, projects, res);
-  } else {
-    status(500, null, res);
+  if (!project) {
+    return next(new AppError("No projects found with this Id", 404));
   }
+  res.status(200).json({
+    status: "success",
+    data: { data: project },
+  });
 });
+
 exports.updateProject = catchAsync(async (req, res, next) => {
   const project = await Project.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
-  if (project) {
-    status(200, projects, res);
-  } else {
-    status(500, null, res);
+  if (!project) {
+    return next(new AppError("No projects found with this Id", 404));
   }
+  res.status(200).json({
+    status: "success",
+    data: { data: project },
+  });
 });
+
 exports.deleteProject = catchAsync(async (req, res, next) => {
   const project = await Project.deleteOne({ _id: req.params.id });
-  if (project.deletedCount > 0) {
-    status(204, null, res);
-  } else {
-    status(500, null, res);
+  if (!project.deletedCount > 0) {
+    return next(new AppError("No projects found with this Id", 404));
   }
+  res.status(204).json({
+    status: "success",
+    data: {},
+  });
 });
 
 exports.addTodoToProject = catchAsync(async (req, res, next) => {
-  if (req.body.todoId) {
-    const project = await Project.findByIdAndUpdate(
-      req.params.id,
-      {
-        $addToSet: { todos: req.body.todoId },
-      },
-      { new: true }
-    );
-    status(201, project, res);
+  const project = await Project.findByIdAndUpdate(
+    req.params.id,
+    {
+      $addToSet: { todos: req.body.todoId },
+    },
+    { new: true }
+  );
+  if (!project) {
+    return next(new AppError("No Todo found with this Id", 404));
   }
+  res.status(201).json({
+    status: "success",
+    data: { data: project },
+  });
 });
 
 exports.replaceTodoFromProject = catchAsync(async (req, res, next) => {
-  if (req.body.todoId) {
-    const project = await Project.findByIdAndUpdate(
-      req.params.id,
-      {
-        $pull: { todos: req.body.todoId },
-      },
-      { new: true }
-    );
-    status(201, project, res);
+  const project = await Project.findByIdAndUpdate(
+    req.params.id,
+    {
+      $pull: { todos: req.body.todoId },
+    },
+    { new: true }
+  );
+  if (!project) {
+    return next(new AppError("No Todo found with this Id", 404));
   }
+  res.status(201).json({
+    status: "success",
+    data: { data: project },
+  });
 });
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,87 +1,77 @@
 const Todo = require("./../models/todoModel");
 const catchAsync = require("./../utils/catchAsync");
-var ObjectID = require("mongodb").ObjectID;
-
-const status = async (statusCode, data, res) => {
-  if (statusCode === 404) {
-    res.status(statusCode).json({
-      status: "fail",
-    });
-  } else {
-    res.status(statusCode).json({
-      status: "success",
-      data,
-    });
-  }
-};
+const AppError = require("./../utils/appError");
 
 exports.getTodos = catchAsync(async (req, res, next) => {
-  const todo = await Todo.find();
-  if (todo) {
-    status(200, todo, res);
-  } else {
-    status(404, null, res);
+  const todos = await Todo.find();
+  if (!todos) {
+    return next(new AppError("No Todos found with this Id", 404));
   }
+  res.status(200).json({
+    status: "success",
+    data: { data: todos },
+  });
 });
 
 exports.getTodo = catchAsync(async (req, res, next) => {
   const todo = await Todo.findById(req.params.id);
-  if (todo) {
-    status(200, todo, res);
-  } else {
-    status(404, null, res);
+  if (!todo) {
+    return next(new AppError("No Todos found with this Id", 404));
   }
+  res.status(200).json({
+    status: "success",
+    data: { data: todo },
+  });
 });
 
 exports.deleteTodo = catchAsync(async (req, res, next) => {
   const todo = await Todo.deleteOne({ _id: req.params.id });
   console.log(todo);
-  if (todo.deletedCount > 0) {
-    status(204, null, res);
-  } else {
-    status(404, null, res);
+  if (!todo.deletedCount > 0) {
+    return next(new AppError("No Todos found with this Id", 404));
   }
+  res.status(204).json({
+    status: "success",
+    data: {},
+  });
 });
 
 exports.createTodo = catchAsync(async (req, res, next) => {
-  if (req) {
-    // Adds a todo
-    var todo = await Todo.create(req.body);
-
-    if (todo) {
-      status(201, todo, res);
-    } else {
-      status(404, null, res);
-    }
-  }
+  var todo = await Todo.create(req.body);
+  res.status(201).json({
+    status: "success",
+    data: { data: todo },
+  });
 });
 
 exports.flipStatus = catchAsync(async (req, res, next) => {
-  if (req) {
-    const todo = await Todo.findByIdAndUpdate(
-      req.params.id,
-      {},
-      { new: true },
-      function (err, val) {
-        val.completed = !val.completed;
-        val.save();
-        res.status(200).json({
-          data: val,
-        });
+  const todo = await Todo.findByIdAndUpdate(
+    req.params.id,
+    {},
+    { new: true },
+    function (err, val) {
+      if (!val) {
+        return next(new AppError("No Todos found with this Id", 404));
       }
-    );
-  }
+      val.completed = !val.completed;
+      val.save();
+      res.status(200).json({
+        status: "success",
+        data: { data: val },
+      });
+    }
+  );
 });
 
 exports.updateTodo = catchAsync(async (req, res, next) => {
-  if (req) {
-    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    if (todo) {
-      status(200, todo, res);
-    } else {
-      status(404, null, res);
-    }
+  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  if (!todo) {
+    return next(new AppError("No Todos found with this Id", 404));
   }
+  res.status(200).json({
+    status: "success",
+    data: { data: todo },
+  });
 });
